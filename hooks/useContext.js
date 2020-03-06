674e7133e80a6ec4513a077290d0aa9c2ca3e93c
Alex Saunders <acsauk@gmail.com>
2020-03-06 12:40:38 +0000

    revert useContext

--- hooks/useContext.js
+++ hooks/useContext.js
@@ -1,28 +1,46 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useContext } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const CountContext = React.createContext()
+
+function CountNumber() {
+    const { count } = useContext(CountContext)
+
+    return <span style={{ color: "hotpink" }}>{count}</span>
+}
+
+function CountTitle() {
+    return (
+        <h1>
+            The current count is <CountNumber />
+        </h1>
+    )
+}
+
 function App() {
-    const [value, setValue] = useState('');
-    const messageRef = useRef()
+    const [count, setCount] = useState(0);
+    
+    function increment(e) {
+        e.preventDefault()
+        setCount(oldCount => oldCount + 1)
+    }
 
-    useEffect(() => {
-        console.log(messageRef)
-    })
-
-    function handleInput(e) {
-        setValue(e.target.value)
+    function reset(e) {
+        e.preventDefault();
+        setCount(0)
     }
 
     return (
+      <CountContext.Provider value={{count}}>
           <div className="App">
-            <h1>How to useRef in React</h1>
-            <input value={value} onChange={handleInput} />
-            <div>
-                <span>{value}</span>
-            </div>
+            <h1>How to useContext in React</h1>
+            <button onClick={increment}>Increase Count</button>
+            <button onClick={reset}>Reset Count</button>
+            <CountTitle />
           </div>
+      </CountContext.Provider>
     );
 }
 
